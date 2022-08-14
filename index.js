afa65566dbb1ccabc664e3d51d9cb1128cf5d683
Merit Dike <65596735+Meritdike@users.noreply.github.com>
2022-08-14 22:22:22 +0100

    ch: added an update name feature when starting a new game

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let printMessage = ''
 let hasBlackjack = false; //has no blackjack ie 21 before the start of the game
 let isAlive = false; //is not alive before the game starts
 let player = {
-    name: 'MERIT',
+    name: 'Enter your name',
     chips: 250
 }
 
@@ -47,6 +47,12 @@ function startGame() {
 
     renderCards()
 
+    if (player.name == 'Enter your name') {
+        let userName = prompt('What is your name?')
+        player.name = userName;
+        playerChips.textContent = player.name + ': $' + player.chips
+    } 
+
     if(isAlive === true && sum === 21) {
         player.chips += 7; 
         playerChips.textContent = player.name + ': $' + player.chips
@@ -59,7 +65,13 @@ function getRandomCard() {
     if(getRamdomCard > 10) {
         return 10
     } else if(getRamdomCard === 1) {
-        return (prompt('Do you want 11 or 1')) //Ace is either 1 or 11 as chosen by the player
+       let aceValue = prompt('Do you want 11 or 1') //Ace is either 1 or 11 as chosen by the player
+        if (aceValue === '' || aceValue > 1 || aceValue < 11) {
+            alert('You must enter either 1 or 11')
+            return 0;
+        } else {
+            return aceValue;
+        }
     } else {
         return getRamdomCard
     }
@@ -90,8 +102,14 @@ function newGame() {
     hasBlackjack = false; //has no blackjack ie 21 before the start of the game
     isAlive = false; //is not alive before the game starts
     player = {
-        name: 'MERIT',
+        name: 'Enter your name',
         chips: 250
+    }
+
+    let name = prompt('What is your name?');
+    player.name = name;
+    if (name == '') {
+        return alert('You must enter a name to start a new game!');
     }
 
     cardSum.textContent = 'Sum: '
